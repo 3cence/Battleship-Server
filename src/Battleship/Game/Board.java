863369afa6f25cf8b6e353b1f778528395639fb5
Emma <emmaalderson24@gmail.com>
2023-03-04 17:01:30 -0500

    Worked on placing ships

--- src/Battleship/Game/Board.java
+++ src/Battleship/Game/Board.java
@@ -1,11 +1,36 @@
 package Battleship.Game;
 
+import Network.PacketData;
+
+import java.util.List;
+
 public class Board {
     private static final int size = 10;
-    private Tile[][] targetBoard, shipBoard;
+    private static final int ships = 5;
+    private Tile[][] shipBoard;
 
     public Board() {
-        targetBoard = new Tile[10][10];
         shipBoard = new Tile[10][10];
+    }
+    public void placeShips(List<PacketData> shipPacket) {
+        PacketData headerPacket = shipPacket.remove(0);
+        if (!headerPacket.type().equals("ship_placement") || !headerPacket.data().equals("" + ships))
+            throw new RuntimeException("This is not a valid ship placement packet, has type " + headerPacket.type() + " & data " + headerPacket.data());
+        for (PacketData ship: shipPacket) {
+            ShipType type = ShipType.fromString(ship.type());
+            // 0 = x, 1 = y, 2 = horizontal/vertical
+            String[] positionInfo = ship.data().split(",");
+            int x = Integer.parseInt(positionInfo[0].substring(1));
+            int y = Integer.parseInt(positionInfo[1].substring(1));
+            boolean isHorizontal = positionInfo[2].equals("horizontal");
+            for (int i = 0; i < type.holes(); i++) {
+                if (isHorizontal) {
+                    // TODO: Set tiles accordingly
+                }
+                else {
+
+                }
+            }
+        }
     }
 }
--- src/Battleship/Game/ShipType.java
+++ src/Battleship/Game/ShipType.java
@@ -1,9 +1,25 @@
 package Battleship.Game;
 
-public record ShipType(String name, int holes) {
+public record ShipType(String type, int holes) {
     public static final ShipType CARRIER = new ShipType("carrier", 5);
     public static final ShipType BATTLESHIP = new ShipType("battleship", 4);
     public static final ShipType CRUISER = new ShipType("cruiser", 3);
     public static final ShipType SUBMARINE = new ShipType("submarine", 3);
     public static final ShipType DESTROYER = new ShipType("destroyer", 2);
+    public static ShipType fromString(String s) {
+        switch (s) {
+            case "carrier":
+                return ShipType.CARRIER;
+            case "battleship":
+                return ShipType.BATTLESHIP;
+            case "cruiser":
+                return ShipType.CRUISER;
+            case "submarine":
+                return ShipType.SUBMARINE;
+            case "destroyer":
+                return ShipType.DESTROYER;
+            default:
+                throw new RuntimeException(s + " is not a ship type!");
+        }
+    }
 }
